e11887a1b52603efa5d81002a1cf1d23fe37313d
forward <yangyangmyo1994@gmail.com>
2026-05-06 20:55:22 +0800

    chore: repo update

--- bot/src/main/java/com/backend/bot/repository/BotGroupTopicRepository.java
+++ bot/src/main/java/com/backend/bot/repository/BotGroupTopicRepository.java
@@ -1,6 +1,7 @@
 package com.backend.bot.repository;
 
 import com.backend.bot.entity.BotGroupTopicEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
