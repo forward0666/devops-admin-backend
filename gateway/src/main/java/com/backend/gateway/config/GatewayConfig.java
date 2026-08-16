8fb273fecf2c1a5c032dd7163aff0f9c178b16ef
DevOps Admin <admin@devops.local>
2026-08-16 06:12:29 +0000

    fix: add GatewayConfig to import ThreadPoolConfig from utils-webflux

---
+++ gateway/src/main/java/com/backend/gateway/config/GatewayConfig.java
@@ -0,0 +1,10 @@
+package com.backend.gateway.config;
+
+import config.ThreadPoolConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(ThreadPoolConfig.class)
+public class GatewayConfig {
+}
